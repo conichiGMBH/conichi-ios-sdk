41f3adb67930becbe91366c826ceb4cd20201962
Anton Domashnev <antondomashnev@gmail.com>
2016-11-28 17:31:54 +0100

    Update bundle;

---
+++ Frameworks/CNISDKCoreKit.framework/PrivateHeaders/CNISDKNSURLSessionConfiguration+WithoutCache.h
@@ -0,0 +1,16 @@
+//
+//  CNISDKNSURLSessionConfiguration+WithoutCache
+//  CNISDKCoreKit
+//
+//  Created by WLADYSLAW SURALA on 18/11/2016.
+//  Copyright © 2016 conichi. All rights reserved.
+//
+
+@import Foundation;
+
+
+@interface NSURLSessionConfiguration (WithoutCache)
+
++ (NSURLSessionConfiguration *)cnisdk_DefaultSessionConfigurationWithoutCache;
+
+@end
